7bacbebe2e53117e51c9aa25ef66b6726414c8b8
Thomas Draper <tgdrape@gmail.com>
2024-12-21 07:58:14 -0800

    Add bitwise z-rotation with tests

--- src/bitcube.rs
+++ src/bitcube.rs
@@ -8,15 +8,8 @@ use std::fmt;
 // Position at (x,y,z) = x + 4*y + 16*z
 // Rotations will happen from the center of the cube
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone)]
 pub struct BitCube4(u64);
-
-#[inline]  // TODO: Make this general for all u16, u32, u64, u128
-fn swap_mask_shift_u32(y: &mut u32, mask: u32, shift: usize) -> () {
-   *y ^= (*y >> shift) & mask;
-   *y ^= (*y & mask) << shift;
-   *y ^= (*y >> shift) & mask;
-}
 
 // impl From<BitPermPoly3> for BitPermTT3 {
     // fn from(bpp3: BitPermPoly3) -> Self {
@@ -31,7 +24,31 @@ fn swap_mask_shift_u32(y: &mut u32, mask: u32, shift: usize) -> () {
     // }
 // }
 
+#[inline]  // TODO: Make this general for all u16, u32, u64, u128
+fn swap_mask_shift_u64(y: &mut u64, mask: u64, shift: usize) -> () {
+   *y ^= (*y >> shift) & mask;
+   *y ^= (*y & mask) << shift;
+   *y ^= (*y >> shift) & mask;
+}
+
 impl BitCube4 {
+    pub fn rotate_x(self) -> Self { todo!() }
+    pub fn rotate_y(self) -> Self { todo!() }
+    // 23 32 31
+    // 01 10 20
+    pub fn rotate_z(self) -> Self { 
+        let mut cube = self.0;
+        // Swap 2x2 squares left <-> right
+        swap_mask_shift_u64(&mut cube, 0x3333_3333_3333_3333_u64, 2);
+        // Swap diagonal 2x2 squares
+        swap_mask_shift_u64(&mut cube, 0x0033_0033_0033_0033_u64, 10);
+        // Within 2x2 squares swap left <-> right columns
+        swap_mask_shift_u64(&mut cube, 0x5555_5555_5555_5555_u64, 1);
+        // Within 2x2 squares swap diagonal entries
+        swap_mask_shift_u64(&mut cube, 0x0505_0505_0505_0505_u64, 5);
+        BitCube4(cube)
+    }
+
     pub fn shift_x(self, shift: i8) -> Self { 
         match shift {
             1 => Self((self.0 << 1) & 0xeeee_eeee_eeee_eeee_u64),
@@ -45,38 +62,12 @@ impl BitCube4 {
     }
     // pub fn shift_y(self, i8) -> Self { todo!() }
     // pub fn shift_z(self, i8) -> Self { todo!() }
-    pub fn rotate_x(self) -> Self { todo!() }
-    pub fn rotate_y(self) -> Self { todo!() }
-    pub fn rotate_z(self) -> Self { 
-        // Swap 2x2 squares left <-> right
-        // Swap diagonal 2x2 squares
-        // Within 2x2 squares swap left <-> right columns
-        // Within 2x2 squares swap diagonal entries
-        todo!() 
-    }
-        
-
-    pub fn compose(self, other: Self) -> Self {
-        Self(
-            (0..8)
-            .map(|x| ((self.0 >> (((other.0 >> (x<<2)) & 7) << 2)) & 7) << (x<<2))
-            .sum()
-            )
-    }
-
-    pub fn inverse(self) -> Self {
-        Self(
-            (0..8)
-            .map(|x| x << (((self.0 >> (x<<2)) & 7) << 2))
-            .sum()
-            )
-    }
 }
 
 
 impl fmt::Debug for BitCube4 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "BitMatrix4({:#06x})", self.0)
+        write!(f, "BitCube4({:#018x})", self.0)
     } 
 } 
 
@@ -84,7 +75,7 @@ impl fmt::Display for BitCube4 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", 
             (0..4)
-            .map(|x| format!("{:#06b}\n", (self.0 >> ((3-x)<<2)) & 0xf))
+            .map(|x| format!("{:#064b}\n", (self.0 >> ((3-x)<<2)) & 0xf))
             .collect::<String>()
             // .join("")
             )
@@ -95,11 +86,24 @@ impl fmt::Display for BitCube4 {
 mod test {
     use super::*;
 
+    const full: BitCube4 = BitCube4(0xffff_ffff_ffff_ffff_u64);
+    const center_x: BitCube4 = BitCube4(0x0000_0ff0_0ff0_0000_u64);
+    const center_y: BitCube4 = BitCube4(0x0000_6666_6666_0000_u64);
+    const center_z: BitCube4 = BitCube4(0x0660_0660_0660_0660_u64);
+
     #[test]
     fn test_shift_x() {
-        assert_eq!(BitCube4(0xffff_ffff_ffff_ffff_u64).shift_x(1), 
+        assert_eq!(full.shift_x(1), 
                    BitCube4(0xeeee_eeee_eeee_eeee_u64)
                    );
+    }
+
+    #[test]
+    fn test_rotate_z() {
+        assert_eq!(full.rotate_z(), full);
+        assert_eq!(center_x.rotate_z(), center_y);
+        assert_eq!(center_y.rotate_z(), center_x);
+        assert_eq!(center_z.rotate_z(), center_z);
     }
 
 }
